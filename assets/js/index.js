083319e224dcf9c76398c3d3d7fc44001c66e9f1
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-21 00:18:02 -0400

    Added dependencies , and search form handler

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,16 @@
-const searchForm = document.querySelector("#searchForm");
-const searchInput = document.querySelector("#searchInput");
-const searchResultList = document.querySelector("#searchResultList");
-const cardContainer = document.querySelector("#cardContainer");
+// Dependencies
+const inputSearch = document.getElementById("searchInput");
+const searchForm = document.getElementById("searchForm");
+const searchResultList = document.getElementById("searchResultList");
+const prevSearch = document.getElementById("prevSearch");
+const forecastEl = document.getElementById("cardContainer");
+const apiKey = "81f5de324935a1e9a4ba6383132f2d99";
 
-// const apiUrl = `api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=f6e57c4912c34be01153c0ddd7de17be`;
-// const apiUrl2 = `http://api.openweathermap.org/geo/1.0/direct?q=${City}&limit=5&appid=f6e57c4912c34be01153c0ddd7de17be`;
-
-const handleSearchClick = (e) => {
+// Search form handler
+searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  localStorage.setItem("search", []);
-  userInput = searchInput.value;
-};
-
-searchForm.addEventListener("submit", handleSearchClick);
+  const city = inputSearch.value.trim();
+  if (city) {
+    fetchCityCoordinates(city);
+  }
+});
